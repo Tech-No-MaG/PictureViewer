5e6378b9d0899ec0c076cca187d79e2776ee4e7b
Tech-No-MaG <step.vow02@yandex.ru>
2026-05-21 23:06:16 +0700

    Изменение названий кнопок

--- PictureViewer/Picture Viewer.Designer.cs
+++ PictureViewer/Picture Viewer.Designer.cs
@@ -32,10 +32,10 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.backgroundButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
@@ -83,10 +83,10 @@
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.AutoSize = true;
-            this.flowLayoutPanel1.Controls.Add(this.button1);
-            this.flowLayoutPanel1.Controls.Add(this.button2);
-            this.flowLayoutPanel1.Controls.Add(this.button3);
-            this.flowLayoutPanel1.Controls.Add(this.button4);
+            this.flowLayoutPanel1.Controls.Add(this.showButton);
+            this.flowLayoutPanel1.Controls.Add(this.clearButton);
+            this.flowLayoutPanel1.Controls.Add(this.backgroundButton);
+            this.flowLayoutPanel1.Controls.Add(this.closeButton);
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(158, 471);
@@ -94,46 +94,46 @@
             this.flowLayoutPanel1.Size = new System.Drawing.Size(875, 47);
             this.flowLayoutPanel1.TabIndex = 2;
             // 
-            // button1
+            // showButton
             // 
-            this.button1.AutoSize = true;
-            this.button1.Location = new System.Drawing.Point(711, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(161, 26);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Показать рисунок";
-            this.button1.UseVisualStyleBackColor = true;
+            this.showButton.AutoSize = true;
+            this.showButton.Location = new System.Drawing.Point(711, 3);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(161, 26);
+            this.showButton.TabIndex = 0;
+            this.showButton.Text = "Показать рисунок";
+            this.showButton.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // clearButton
             // 
-            this.button2.AutoSize = true;
-            this.button2.Location = new System.Drawing.Point(549, 3);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(156, 26);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Очистить рисунок";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.clearButton.AutoSize = true;
+            this.clearButton.Location = new System.Drawing.Point(549, 3);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(156, 26);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Очистить рисунок";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // backgroundButton
             // 
-            this.button3.AutoSize = true;
-            this.button3.Location = new System.Drawing.Point(387, 3);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(156, 26);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Задать цвет фона";
-            this.button3.UseVisualStyleBackColor = true;
+            this.backgroundButton.AutoSize = true;
+            this.backgroundButton.Location = new System.Drawing.Point(387, 3);
+            this.backgroundButton.Name = "backgroundButton";
+            this.backgroundButton.Size = new System.Drawing.Size(156, 26);
+            this.backgroundButton.TabIndex = 2;
+            this.backgroundButton.Text = "Задать цвет фона";
+            this.backgroundButton.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // closeButton
             // 
-            this.button4.AutoSize = true;
-            this.button4.Location = new System.Drawing.Point(306, 3);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 26);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Закрыть";
-            this.button4.UseVisualStyleBackColor = true;
+            this.closeButton.AutoSize = true;
+            this.closeButton.Location = new System.Drawing.Point(306, 3);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 26);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "Закрыть";
+            this.closeButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -159,10 +159,10 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button backgroundButton;
+        private System.Windows.Forms.Button closeButton;
     }
 }
 
